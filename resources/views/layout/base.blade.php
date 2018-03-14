635a5256c1a7fe6ce746fb0b19f3262bb6dc731a
andrei.popa <apopa@optaros.com>
2018-03-14 11:49:43 +0200

    Disallowed indexing

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <meta name="googlebot" content="noindex, nofollow">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
