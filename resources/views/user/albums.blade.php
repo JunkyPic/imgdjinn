6dde5c686403d4f2cc6a83d1f85ab64bf6297f8c
Andrei Popa <junkypic@gmail.com>
2018-03-15 06:06:08 +0200

    Added various info for images and album

--- resources/views/user/albums.blade.php
+++ resources/views/user/albums.blade.php
@@ -5,23 +5,35 @@
 @endsection
 
 @section('content')
-    @foreach($albums->chunk(2) as $chunk)
-        {{-- get and display only the first image--}}
-        <div class="row splitter">
-            @foreach($chunk as $album)
-                <div class="col-lg-6 text-center">
-                    <small>Token: {{ null !== $album->token ? $album->token : 'No token found' }}</small>
-                    <a href="{{ route('showAlbum', ['alias' => $album->alias]) }}">
-                        <img class="img-responsive rounded img-a" src="{{  url('/img/' . $album->images()->first()->path ) }}">
-                    </a>
-                </div>
-            @endforeach
+    @if($albums->count() == 0)
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="display-5 text-center text-muted">No albums found</h2>
+            </div>
         </div>
-    @endforeach
+    @else
+        @foreach($albums->chunk(2) as $chunk)
+            {{-- get and display only the first image--}}
+            <div class="row splitter">
+                @foreach($chunk as $album)
+                    <div class="col-lg-6 text-center">
+                        <div class="col-lg-12">
+                            <span class="badge badge-info">Has password - <small>{{ null === $album->password ? 'No' : 'Yes' }}</small></span>
+                            <span class="badge badge-info">Token - <small>{{ null === $album->token ? 'No token found' :  $album->token}}</small></span>
+                        </div>
+                        <a href="{{ route('showAlbum', ['alias' => $album->alias]) }}">
+                            <img class="img-responsive rounded img-a" src="{{  url('/img/' . $album->images()->first()->path ) }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
 
-    <div class="row">
-        <div class="col-lg-12 pagination pagination-centered justify-content-center">
-            {{ $albums->links() }}
+        <div class="row">
+            <div class="col-lg-12 pagination pagination-centered justify-content-center">
+                {{ $albums->links() }}
+            </div>
         </div>
-    </div>
+    @endif
+
 @endsection
--- resources/views/user/images.blade.php
+++ resources/views/user/images.blade.php
@@ -5,23 +5,35 @@
 @endsection
 
 @section('content')
-    @foreach($images->chunk(2) as $chunk)
-        {{-- get and display only the first image--}}
-        <div class="row splitter">
-            @foreach($chunk as $image)
-                <div class="col-lg-6 text-center">
-                    <small>Token: {{ null !== $image->token ? $image->token : 'No token found' }}</small>
-                    <a href="{{ route('showImage', ['alias' => $image->alias]) }}">
-                        <img class="img-responsive rounded img-a" src="{{  url('/img/' . $image->path ) }}">
-                    </a>
-                </div>
-            @endforeach
+    @if($images->count() == 0)
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="display-5 text-center text-muted">No images found</h2>
+            </div>
         </div>
-    @endforeach
+    @else
+        @foreach($images->chunk(3) as $chunk)
+            {{-- get and display only the first image--}}
+            <div class="row splitter">
+                @foreach($chunk as $image)
+                    <div class="col-lg-4 text-center">
+                        <div class="col-lg-12">
+                            <span class="badge badge-info">Has password - <small>{{ null === $image->password ? 'No' : 'Yes' }}</small></span>
+                            <span class="badge badge-info">Token - <small>{{ null === $image->token ? 'No token found' :  $image->token}}</small></span>
+                        </div>
+                        <a href="{{ route('showImage', ['alias' => $image->alias]) }}">
+                            <img class="img-responsive rounded img-a" src="{{  url('/img/' . $image->path ) }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
 
-    <div class="row">
-        <div class="col-lg-12 pagination pagination-centered justify-content-center">
-            {{ $images->links() }}
+        <div class="row">
+            <div class="col-lg-12 pagination pagination-centered justify-content-center">
+                {{ $images->links() }}
+            </div>
         </div>
-    </div>
+    @endif
+
 @endsection
