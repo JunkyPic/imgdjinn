b38f59dabf78200b3131ab6b115b055090c93d9d
andrei.popa <apopa@optaros.com>
2018-03-16 10:43:35 +0200

    temp header

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ImagesResource;
 use App\Models\Album;
 use App\Models\Image;
+use Illuminate\Http\JsonResponse;
 
 /**
  * Class ImageController
@@ -15,20 +16,29 @@ use App\Models\Image;
  */
 class ImageController extends Controller
 {
-    /**
-     * @param $alias
-     *
-     * @return ImageResource
-     */
+
+  /**
+   * @param $alias
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
     public function apiGetImage($alias) {
         ImageResource::withoutWrapping();
-        return new ImageResource(Image::where(['alias' => $alias])->select('path')->first());
+
+      $image = Image::where(['alias' => $alias])->select('path')->first();
+
+      return new JsonResponse($image->path, 200, ['Access-Control-Allow-Origin' => '*']);
+
+//        return new ImageResource();
     }
 
     public function apiGetAlbumImages($alias) {
         ImageResource::withoutWrapping();
 
         $album = Album::where(['alias' => $alias])->first();
+
+
+
 
         return new ImagesResource($album->images()->get());
     }
