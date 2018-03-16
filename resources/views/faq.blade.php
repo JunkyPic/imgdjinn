5b04057fd82f02aaae2180ac1e8c4e4996717f3a
Andrei Popa <junkypic@gmail.com>
2018-03-16 20:23:31 +0200

    updated faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -12,10 +12,9 @@
             <p>No, currently there isn't any upload limit. However please upload within reason. I'm just one guy, I don't have unlimited funds.</p>
             <hr>
             <h2>How can I delete an image I've uploaded?</h2>
-            <p>If you've selected one of the options (auto-generate token or enter a password) then you can use the "Delete?" option at the bottom of either the album or image page.
-            If you didn't select either of the options previously mentioned then I'm afraid there's no way to delete the image(you can send me an email or something and I'll see what I can do - see next question).
-                At some point in time I'll create an option to delete the images/albums if you have a registered account(no email required) without the need for a token or password.
-            </p>
+            <p>If you've selected one of the options (auto-generate token or enter a password) then you can use the "Delete?" option at the bottom of either the album or image page.</p>
+            <p>If you have a registered account(no email required) you can delete images/albums without the need for a token or password.</p>
+            <p>If you didn't select either of the options previously mentioned then I'm afraid there's no way to delete the image(you can send me an email or something and I'll see what I can do - see next question).</p>
             <hr>
             <h2>What's your stance on privacy?</h2>
             <p>I take privacy very seriously. I don't track or ever plan on tracking any activity on this site. There's close to no javascript and the one that is present is there for quality of life.
