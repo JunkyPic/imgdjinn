a0856e217c9d84d23929b918229f87e5a0440ff9
Andrei Popa <junkypic@gmail.com>
2018-03-16 19:14:32 +0200

    reworked parts of config

--- config/image.php
+++ config/image.php
@@ -3,6 +3,10 @@
 return [
   'path' => [
       'upload' => public_path('img/'),
-      'link' => url('img') . '/',
+      'link' => sprintf("%s://%s/callback",
+          isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ?
+              'https' : 'http',
+          $_SERVER['SERVER_NAME']
+      ) . 'img/',
   ]
 ];
