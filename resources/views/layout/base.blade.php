f71312254ee1e680c17d451db2908337c06b8dab
Andrei Popa <junkypic@gmail.com>
2018-03-16 21:50:38 +0200

    added description metatag

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="An image upload site focused on privacy and data retention" />
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-
     <link href="{{ asset('css/slate.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/css.css') }}" rel="stylesheet">
 
